9bd5aa2c425848006139344c759e71dcc1464bb9
michaelosullivan292-max <michaelosullivan292@gmail.com>
2026-02-24 23:53:03 -0800

    Add files via upload

---
+++ module1_raining.py
@@ -0,0 +1,14 @@
+# Program: Raining Cats and Dogs
+# Perpose: Demonstrate variables and printing a sentence
+
+# Variable c stores the word cats.
+c = "cats"
+
+# Variable d stores the word dogs.
+d = "dogs."
+
+# Variable s stors the full sentence.
+s = "It is raining " + c + " and " + d
+
+# Print the final sentence.
+print(s)
